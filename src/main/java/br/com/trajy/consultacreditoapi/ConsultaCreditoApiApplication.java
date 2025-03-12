06d535c2ee68e386a7d2deeda658b3707a0bae35
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-12 08:34:02 -0300

    back-end initial commit

---
+++ src/main/java/br/com/trajy/consultacreditoapi/ConsultaCreditoApiApplication.java
@@ -0,0 +1,16 @@
+package br.com.trajy.consultacreditoapi;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class ConsultaCreditoApiApplication {
+
+	public static void main(String[] args) {
+		run(ConsultaCreditoApiApplication.class);
+	}
+
+}
---
+++ src/test/java/br/com/trajy/consultacreditoapi/ConsultaCreditoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trajy.consultacreditoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaCreditoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

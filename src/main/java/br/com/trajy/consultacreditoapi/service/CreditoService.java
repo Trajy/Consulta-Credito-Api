a9decdcd09d95fdcfa1bc9e3a5e3702e942e3868
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-16 16:51:49 -0300

    fix cant call sendNotification method error

--- src/main/java/br/com/trajy/consultacreditoapi/service/CreditoService.java
+++ src/main/java/br/com/trajy/consultacreditoapi/service/CreditoService.java
@@ -2,10 +2,8 @@ package br.com.trajy.consultacreditoapi.service;
 
 import static br.com.trajy.architecture.restful.exception.utils.ExceptionHandlerUtils.generateEntityNotFoundMessage;
 import static java.time.LocalDateTime.now;
-import static java.util.Objects.nonNull;
 import static org.apache.commons.collections4.CollectionUtils.isEmpty;
 
-import br.com.trajy.architecture.restful.exception.utils.ExceptionHandlerUtils;
 import br.com.trajy.consultacreditoapi.model.entity.AnalyticsModel;
 import br.com.trajy.consultacreditoapi.model.entity.Credito;
 import br.com.trajy.consultacreditoapi.repository.CreditoRepository;
@@ -14,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,20 +23,19 @@ public class CreditoService {
     private final MessageProducerWrapper messageProducer;
 
     public Credito findByNumeroCredito(String numeroCredito) {
-        Credito entity = repository.findByNumeroCredito(numeroCredito)
-                .orElseThrow(() -> new EntityNotFoundException(
-                        generateEntityNotFoundMessage(Credito.class, "numeroCredito", numeroCredito)
-                ));
-        produceMessage(numeroCredito, nonNull(entity));
-        return entity;
+        Optional<Credito> optionalEntity = repository.findByNumeroCredito(numeroCredito);
+        produceMessage(numeroCredito, optionalEntity.isPresent());
+        return optionalEntity.orElseThrow(() -> new EntityNotFoundException(
+                generateEntityNotFoundMessage(Credito.class, "numeroCredito", numeroCredito)
+        ));
     }
 
     public List<Credito> findByNumeroNfse(String numeroNfse) {
         List<Credito> entities = repository.findByNumeroNfse(numeroNfse);
+        produceMessage(numeroNfse, isEmpty(entities));
         if(isEmpty(entities)) {
             throw new EntityNotFoundException(generateEntityNotFoundMessage(Credito.class, "numeroNfse", numeroNfse));
         }
-        produceMessage(numeroNfse, isEmpty(entities));
         return entities;
     }
 

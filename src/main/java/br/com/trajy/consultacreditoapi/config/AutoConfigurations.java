8dc36b3e652733f83f93ff71c5bae795ebcd7e07
Henrique Trajano <henrique.trajano.a@gmail.com>
2025-03-16 14:45:06 -0300

    implement async call to notification method

--- src/main/java/br/com/trajy/consultacreditoapi/config/AutoConfigurations.java
+++ src/main/java/br/com/trajy/consultacreditoapi/config/AutoConfigurations.java
@@ -5,12 +5,14 @@ import br.com.trajy.architecture.restful.exception.RestGlobalExecptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Import({
         RestGlobalExecptionHandler.class,
         OpenApiCoreConfig.class
 })
 @EnableKafka
+@EnableAsync
 @Configuration
 public class AutoConfigurations {
 
--- src/main/java/br/com/trajy/consultacreditoapi/stream/MessageProducerWrapper.java
+++ src/main/java/br/com/trajy/consultacreditoapi/stream/MessageProducerWrapper.java
@@ -2,6 +2,7 @@ package br.com.trajy.consultacreditoapi.stream;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +11,7 @@ public class MessageProducerWrapper {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
+    @Async
     public <T> void send(T message) {
         kafkaTemplate.send("SEARCH_ANALITICS", message);
     }
